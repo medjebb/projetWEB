35af01a40a1c49e99015b1f1c748d530a01382da
Chebila <Chebila.douae@gmail.com>
2023-06-21 02:56:35 +0100

    last update

--- Employe.php
+++ Employe.php
@@ -309,7 +309,7 @@
                     <!-- Card Header - Dropdown -->
                     <div class="card shadow mb-4">
                             <div class="card-header py-3">
-                                <h6 class="m-0 font-weight-bold text-primary">Nouveau taches</h6>
+                                <h6 class="m-0 font-weight-bold " style="color:#ff5e37">Nouvelles taches</h6>
                             </div>
                             <div class="card-body">
                                 <div class="text-center">
@@ -339,7 +339,7 @@
                      <!-- Approach -->
                      <div class="card shadow mb-4">
                             <div class="card-header py-3">
-                                <h6 class="m-0 font-weight-bold text-primary">LMAD</h6>
+                                <h6 class="m-0 font-weight-bold " style="color:#ff5e37">LMAD</h6>
                             </div>
                             <div class="card-body">
                                 <p>LMAD est une application web innovante conçue pour simplifier 
